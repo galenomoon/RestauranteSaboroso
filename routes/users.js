bf45fef50ca3df227133a3d92f89f1115c7a5bc6
galeno <gui.galenocover@gmail.com>
2021-12-30 10:01:18 -0300

    Index.ejs Conversando com Banco de Dados

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,11 @@ router.get('/', function(req, res, next) {
     conn.query("SELECT * FROM tb_users ORDER BY name", (err, result) => {
 
         if (err) {
-
             res.send(err);
+        } else {
+            res.send(result);
+
         }
-        res.send(result);
     })
 });
 
