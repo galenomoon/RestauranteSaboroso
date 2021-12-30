c74afd8a639739987e7bc2d183e0a1fdcbf1c871
galeno <gui.galenocover@gmail.com>
2021-12-30 09:16:37 -0300

    Ponto de partida

---
+++ inc/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'User',
+    database: 'saboroso',
+    password: '@mySQL52931872908'
+});
+module.exports = connection;
