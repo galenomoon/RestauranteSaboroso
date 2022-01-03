44d6de47582efd3c5cdd3ed22d5d2406b3311776
galeno <gui.galenocover@gmail.com>
2022-01-03 09:28:19 -0300

    Enviando Contato com Método POST para o MySQL

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
 var reservations = require('../inc/reservations');
+var contacts = require('../inc/contacts');
 var router = express.Router();
 
 /* GET home page. */
@@ -17,13 +18,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-    res.render('contacts', {
-        title: 'Contato - Restaurante Saboroso!',
-        background: 'images/img_bg_3.jpg',
-        h1: 'Diga um oi!'
-    });
-});
 
+    contacts.render(req, res)
+
+});
+router.post('/contacts', function(req, res, next) {
+
+    if (!req.body.name) {
+        contacts.render(req, res, "Digite o nome")
+    } else if (!req.body.email) {
+        contacts.render(req, res, "Digite o email")
+    } else if (!req.body.message) {
+        contacts.render(req, res, "Digite a mensagem")
+    } else {
+        contacts.save(req.body).then(results => {
+            req.body = {}
+            contacts.render(req, res, null, "Mensagem enviada com sucesso")
+        }).catch(err => {
+            contacts.render(req, res, err.message)
+        })
+    }
+
+});
 router.get('/menus', function(req, res, next) {
 
     menus.getMenus().then(results => {
@@ -51,6 +67,8 @@ router.post('/reservations', function(req, res, next) {
         reservations.render(req, res, "Digite a hora")
     } else {
         reservations.save(req.body).then(results => {
+
+            req.body = {}
 
             reservations.render(req, res, null, "Reserva Finalizada com Sucesso")
 
