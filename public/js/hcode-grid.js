6d6f2b15784d474ea92bc440d5976a48c835ee89
galeno <gui.galenocover@gmail.com>
2022-01-04 17:44:18 -0300

    Administração dos usuários criando telas

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -31,6 +31,10 @@ class HcodeGrid {
             formUpdate: "#modal-update form",
             btnUpdate: ".btn-update",
             btnDelete: ".btn-delete",
+            onUpdateLoad: (form, name, data) => {
+                let input = form.querySelector(`[name=${name}]`);
+                if (input) input.value = data[name];
+            }
         }, configs);
 
         this.initButtons();
