2f4797b14ba25a62c9c6114751ab5d9aa9365374
galeno <gui.galenocover@gmail.com>
2022-01-04 15:06:41 -0300

    Reservations Apresentando dados na tela

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var users = require('./../inc/users');
 var router = express.Router();
 var admin = require('./../inc/admin');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 
 
 //Será criado um MIDDLEWARE que ficará responsável por assegurar de que todas as portas (ou rotas) ficaram seguras e só poderam ser acessadas se o usuário estiver logado
@@ -118,10 +119,45 @@ router.delete('/menus/:id', function(req, res, next) {
 
 router.get('/reservations', function(req, res, next) {
 
-    res.render('admin/reservations', admin.getParams(req, {
-        date: {}
-    }))
+    reservations.getReservations().then(data => {
+
+        res.render('admin/reservations', admin.getParams(req, {
+            date: {},
+            data
+        }))
+
+    }).catch(err => {
+        res.send(err)
+    })
+
+
 });
+
+router.post('/reservations', function(req, res, next) {
+
+    reservations.save(req.fields, req.files).then(results => {
+
+        res.send(results)
+
+    }).catch(err => {
+        res.send(err)
+    })
+
+});
+
+router.delete('/reservations/:id', function(req, res, next) {
+
+    reservations.delete(req.params.id).then(results => {
+
+        res.send(results)
+
+    }).catch(err => {
+        res.send(err)
+    })
+
+});
+
+
 router.get('/users', function(req, res, next) {
 
     res.render('admin/users', admin.getParams(req))
